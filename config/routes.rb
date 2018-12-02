cce3981d7c5b507ca2eb66e942b3d535a4cf388e
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-12-02 19:33:49 +0000

    Gerado view de inicio index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'inicio/index'
   devise_for :gestors
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: "home#index"
+    root to: "inicio#index"
 end
