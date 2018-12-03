99ea241911e3e1bb31d8bf4541201a6357cf88a8
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-12-03 21:50:42 +0000

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,7 @@ Rails.application.routes.draw do
   get 'inicio', to: "inicio#index"
   
   devise_for :gestors
-  
-  devise_scope :gestors do
-  get 'sign_in', to: 'devise/sessions#new'
-end
-  
-  
   devise_for :admins
-  
-  devise_scope :admins do
-  get 'sign_in', to: 'devise/sessions#new'
-end
-  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: "inicio#index"
