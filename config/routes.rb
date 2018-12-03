228c5d81191a801bd1d1cdbeb3e71c4dd62a0ad6
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-12-03 18:46:26 +0000

    Renomeado nome das rotas Dashboard para Painel

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'dashboard/dashboard'
+  get 'painel', to: "dashboard#dashboard"
   get 'inicio/index'
+  get 'inicio', to: "inicio#index"
+  
   devise_for :gestors
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: "inicio#index"
 end
+
